b0ea6a3d7b6faf7e108e5a4c9eac0864d008a8b6
moudzx <mohamadmostafa79@gmail.com>
2026-06-23 18:21:30 +0300

    Moving the sun

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # References
 
+https://www.youtube.com/watch?v=o3apbMOfdTI
+
 https://www.w3schools.com/html/html5_canvas.asp
 
 https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
-
--- game.js
+++ game.js
@@ -1,65 +1,86 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
-const height = canvas.height;
-const width = canvas.width;
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+const radius = 20;
+const speed = 200;
 
+class V2{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    add(that){
+        return new V2(this.x + that.x, this.y + that.y);
+    }
+    subtract(that){
+        return new V2(this.x - that.x, this.y - that.y);
+    }
+    scale(s){
+        return new V2(this.x * s, this.y * s);
+    }
+}
 
-// context.moveTo(0, 0);
-// context.lineTo(width, height);
-// context.stroke();
-// &drawLine(x,y)
+let position = new V2(radius + 10, radius + 10);
+let velocity = new V2(0, 0);
+
+function fillCircle(context, position, radius, color = "red"){
+    context.beginPath();
+    context.arc(position.x, position.y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = color;
+    context.fill();
+}
 
 let start;
-let x = width / 2;
-let y = height / 2;
-
-let dx = 100; // pps
-let dy = 100;
-
-function step(timestamp) {
-    if (start === undefined) {
-        start = timestamp;
-    }
-
+function step(timestamp){
+    if (start === undefined) start = timestamp;
     const deltaTime = (timestamp - start) / 1000;
     start = timestamp;
 
-    x += dx * deltaTime;
-    y += dy * deltaTime;
+    // if (x + radius >= width || x - radius <= 0) dx = -dx;
+    // if (y + radius >= height || y - radius <= 0) dy = -dy;
+    // x += dx * deltaTime;
+    // y += dy * deltaTime;
 
-    if (x + radius > width) {
-        x = width - radius;
-        dx = -dx;
-    } else if (x - radius < 0) {
-        x = radius;
-        dx = -dx;
-    }
-
-    if (y + radius > height) {
-        y = height - radius;
-        dy = -dy;
-    } else if (y - radius < 0) {
-        y = radius;
-        dy = -dy;
-    }
+    position = position.add(velocity.scale(deltaTime));
 
     context.clearRect(0, 0, width, height);
-    fillCircle(x, y, radius, "red");
+    fillCircle(context, position, radius, "red");
 
-    requestAnimationFrame(step);
+    window.requestAnimationFrame(step);
 }
 
-requestAnimationFrame(step);
+let directionMap = {
+    "ArrowUp": new V2(0, -speed),
+    "w": new V2(0, -speed),
 
-function fillCircle(x, y, radius, color = "red") {
-context.beginPath();
-context.arc(x, y, radius, 0, 2 * Math.PI);
-context.fillStyle = color;
-context.fill();
-}
+    "ArrowDown": new V2(0, speed),
+    "s": new V2(0, speed),
 
-const radius = 50;
-context.fillRect(0, 0, width, height);
-fillCircle(x, y, radius);
+    "ArrowLeft": new V2(-speed, 0),
+    "a": new V2(-speed, 0),
 
-    
+    "ArrowRight": new V2(speed, 0),
+    "d": new V2(speed, 0)
+};
+
+
+document.addEventListener("keydown", event => {
+    if (event.key in directionMap){
+        velocity = velocity.add(directionMap[event.key]);
+    }
+});
+
+document.addEventListener("keyup", event => {
+    if (event.key in directionMap){
+        velocity = velocity.subtract(directionMap[event.key]);
+    }
+});
+
+document.addEventListener("click", () => {
+    document.getElementById("bgm").play();
+}, { once: true });
+
+window.requestAnimationFrame(step);
